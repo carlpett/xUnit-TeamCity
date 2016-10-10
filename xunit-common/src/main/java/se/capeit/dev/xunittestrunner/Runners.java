be03fa299d8b80e562d7b7f4e719f58b9a27184d
Nick Stenroos-Dam <nsd@workbook.net>
2016-10-10 10:50:56 +0200

    Added xunit 2.2.0-beta3

--- xunit-common/src/main/java/se/capeit/dev/xunittestrunner/Runners.java
+++ xunit-common/src/main/java/se/capeit/dev/xunittestrunner/Runners.java
@@ -72,6 +72,20 @@ public final class Runners {
                 return sb.toString();
             }
         });
+		
+		AvailableRunners.put("2.2.0-beta3", new RunnerVersion("2.2.0-beta3",
+                new String[]{Runtime.dotNET45},
+                new String[]{Platforms.x86, Platforms.MSIL}) {
+            @Override
+            public String getRunnerPath(String runtime, String platform) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("xunit.console");
+                if (platform.equals(Platforms.x86))
+                    sb.append(".x86");
+                sb.append(".exe");
+                return sb.toString();
+            }
+        });
     }
 }
 
